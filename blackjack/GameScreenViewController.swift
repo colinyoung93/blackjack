3de324b5341e5b9f5055b0bd308a9281218a5972
colin <colinyoung93@hotmail.com>
2022-04-13 23:17:25 -0300

    built deck

--- blackjack/GameScreenViewController.swift
+++ blackjack/GameScreenViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-
+class PlayingCard {
+    var value: Int
+    var image: UIImage!
+    init(value: Int, path:String){
+        self.value = value
+        self.image = UIImage(named: path)
+    }
+}
 
 class GameScreenViewController: UIViewController {
 
@@ -18,12 +25,21 @@ class GameScreenViewController: UIViewController {
     @IBOutlet weak var dealerCardImage1: UIImageView!
     @IBOutlet weak var dealerCardImage2: UIImageView!
     
-    
+    var deck = [PlayingCard]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        for x in 1...13{
+            for y in 1...4{
+                if (x < 10){
+                    deck.append(PlayingCard(value:x, path:"" + String(x) + "_" + String(y) +  ".svg"))
+                } else {
+                    deck.append(PlayingCard(value:10, path:"" + String(x) + "_" + String(y) +  ".svg"))
+                }
+            }
+        }
+        playerCardImage1.image = deck[0].image
     }
     
     @IBAction func hitMeButton(_ sender: UIButton) {

db43b3c3df454e2acb3424c6b76909cf40156896
colin <colinyoung93@hotmail.com>
2022-04-14 11:46:43 -0300

    display cards. currently a error

--- blackjack/GameScreenViewController.swift
+++ blackjack/GameScreenViewController.swift
@@ -25,11 +25,31 @@ class GameScreenViewController: UIViewController {
     @IBOutlet weak var dealerCardImage1: UIImageView!
     @IBOutlet weak var dealerCardImage2: UIImageView!
     
+    
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    
     var deck = [PlayingCard]()
+    var dealerScore = 0
+    var playerScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buildAndDeal()
+    }
+    
+    @IBAction func hitMeButton(_ sender: UIButton) {
+        
+    }
+    
+    func updatePlayerScore(score:Int){
+        playerScoreLabel.text = String(score)
+    }
+    
+    
+    func buildAndDeal(){
+        dealerScore = 0
+        playerScore = 0
         for x in 1...13{
             for y in 1...4{
                 if (x < 10){
@@ -39,13 +59,25 @@ class GameScreenViewController: UIViewController {
                 }
             }
         }
+        
+        deck.shuffle()
+        
         playerCardImage1.image = deck[0].image
-    }
-    
-    @IBAction func hitMeButton(_ sender: UIButton) {
+        playerScore += deck[0].value
+        deck.remove(at: 0)
+        playerCardImage2.image = deck[0].image
+        playerScore += deck[0].value
+        deck.remove(at: 0)
+        dealerCardImage1.image = deck[0].image
+        dealerScore += deck[0].value
+        deck.remove(at: 0)
+        dealerCardImage2.image = deck[0].image
+        dealerScore += deck[0].value
+        deck.remove(at: 0)
+        
+        updatePlayerScore(score: playerScore)
         
     }
-    
     /*
     // MARK: - Navigation
 
